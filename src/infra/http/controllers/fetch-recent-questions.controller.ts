d5fd1a77d009c2627282f2ee00028f1533662317
meints <cadumeints0@gmail.com>
2025-07-14 22:18:55 -0300

    cloning/uploading files from the previous project, but with the clean architecture standard

---
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { ZodValidationPipe } from '@/infra/pipes/zod-validation-pipe'
+import { PrismaService } from '@/infra/prisma/prisma.service'
+import z from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const pageValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', pageValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 20
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return { questions }
+  }
+}
